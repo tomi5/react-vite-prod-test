1b6f7b031ccb4fe49c90b63daac8fdb09cfcc99b
Tomi5 <tomasz.rysak@gmail.com>
2025-01-16 20:11:07 +0100

    Add deploy test message to App component

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>DEPLOY COMPLETED</h2>
+      <h2>DEPLOY TEST</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
