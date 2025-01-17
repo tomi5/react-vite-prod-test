3180a959d2161e01b98c9ffd37fbc99be0993fbe
Tomi5 <tomasz.rysak@gmail.com>
2025-01-17 13:37:00 +0100

    chore: switch web server command from npm to yarn in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev',
+    command: 'yarn dev',
     url: 'http://localhost:3000/',
     reuseExistingServer: !process.env.CI,
   },
